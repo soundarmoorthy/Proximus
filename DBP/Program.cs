1074965e71ccaa48f5e8bf0c04e708d2dca537cd
Satheesh, Sandeep <SatheesS@advisory.com>
2019-07-21 18:03:17 +0530

    Add project files.

---
+++ DBP/Program.cs
@@ -0,0 +1,29 @@
+﻿using Neo4j.Driver.V1;
+using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DBP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args[0];
+
+
+            var geoHashPopulator = new GeoHashPopulator();
+
+            using (var rd = new StreamReader(file))
+            {
+
+                while (!rd.EndOfStream)
+                {
+                    var geoHash = rd.ReadLine();
+                    geoHashPopulator.InsertIntoDbAsync(geoHash);                                       
+                }
+            }
+        }
+    }
+}
